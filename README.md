5231d34382c2753555f639537f287dee9b22448d
Aljaz Krasevec <hip3r.master@gmail.com>
2016-06-14 21:06:25 +0200

    example

--- README.md
+++ README.md
@@ -10,15 +10,19 @@ AngularJS directive for Googlebot integration
  This service detects if Googlebot is crawling the page. It searches through all states, finds the right one and redirects to it.  
  
  Note: You must enable hashprefix in order to comply with Google standards ( .config([...]){ $locationProvider.hashPrefix('!'); }))  
- 
- 1.) Change first line of javascript to comply with your format of the app.
- 2.) Add service name (gBot) where you run the app and initiate it on "$stateChangeStart" event. The skeleton would look like this:
- 
- .run(['$window', '$rootScope', '$state', '$stateParams', 'gBot', function($window, $rootScope, $state, $stateParams, gBot) {  
- /*** some code ***/  
- 
-   $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {  
-     if(gBot.isActive(event)) return;  
-   });  
- 
- }]);
+and don't forget to put fragment meta tag: <meta name="fragment" content="!">  
+   
+ 1.) Include "googleBot" as a dependancy in your Angular application   
+ 2.) Add service name (gBot) where you run the app   
+ 3.) Service has one function, named "isActive(event)". If it returns true, than google crawler has been detected and state change has already been initialized. 
+ All you need to do, is stop the original "$stateChangeStart" event  
+  
+-take a look at example: app.js file  
+  
+#Testing
+Google crawler changes hashtag into GET parameter "_escaped_fragment_". Convert your link accordingly, example:  
+http://127.0.0.1/gBot/example/#!/messages  
+becomes  
+http://127.0.0.1/gBot/example/?_escaped_fragment_=/messages  
+  
+You should be redirected to substate instead of default.
---
+++ example/app.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tvdApp
+ * @description
+ * # tvdApp
+ *
+ * Main module of the application.
+ */
+angular.module('myApp', [
+    'ui.router',
+	'googleBot',
+])
+.config(['$locationProvider', '$urlRouterProvider', '$stateProvider', function ($locationProvider, $urlRouterProvider, $stateProvider) {
+    $locationProvider.html5Mode(false);
+    $locationProvider.hashPrefix('!');
+    $urlRouterProvider.otherwise('/');
+	
+
+	
+	// Routes
+	$stateProvider
+		.state('home', {
+			url: '/',
+			template: '<div></div>'
+		})
+		.state('notifications', {
+			url: '^/notifications',
+			template: '<div><h1>Notifications</h1><p>List of notifications is empty.</p><small>This is your default view</small><button ui-sref="messages()">Next</button></div>',
+		})
+		.state('messages', {
+			url: '^/messages',
+			template: '<div><h1>Messages</h1><p>You have no new messages</p><small>This is substate</small><button ui-sref="messages()">Prev</button></div>',
+		});
+
+}])
+.run(['$window', '$rootScope', '$state', '$stateParams', 'gBot', function($window, $rootScope, $state, $stateParams, gBot) {
+	$rootScope.$state = $state;
+	$rootScope.$stateParams = $stateParams;
+	
+
+	$state.previous = $state.current;
+	$state.previous.params = $stateParams; 
+	
+
+
+	$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+
+		
+		if(gBot.isActive(event)) return;
+		else if (toState.name === 'home') {
+			event.preventDefault();
+			$state.go('notifications');
+			return;
+		}
+		
+	});
+
+	$rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+	  $state.previous = fromState;
+	  $state.previous.params = fromParams;
+	});
+
+	$rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+		event.preventDefault();
+		console.log(error);
+	});
+	
+	
+
+
+}]);
+  
+
+angular.element(document).ready(function () {
+	
+	angular.bootstrap(document, ['myApp']);
+});
